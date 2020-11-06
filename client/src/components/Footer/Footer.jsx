1743e7dad02e67992fa2dd716d7ab0e0a69e138b
Eduardo Pineda <edibozu@gmail.com>
2020-11-06 17:39:33 -0500

    deleted unnecessary files and created footer. installed react-router-dom

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Footer.css"
+
+const Footer = () => {
+    return (
+        <div>
+            <div className="card-footer text-muted text-center" id="footer">
+                <p>Copyright <i className="far fa-copyright" id="cr-text"> The Borons 2020, All Rights Reserved.</i></p>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
