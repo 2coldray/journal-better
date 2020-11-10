9564b7783ab63c965d2a25148ab8a2b0d62d0b19
Jon Peach <jonmakesitbetter@gmail.com>
2020-11-09 19:16:41 -0500

    Built the bones of the Week.jsx page

---
+++ client/src/pages/Week.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import "./pages.css";
+import "../components/Footer";
+
+const Week = () => {
+  return (
+    <div>
+      <div class="row">
+        <div class="col-md-3">
+          <div class="card-body">
+            <h5 class="card-title">Day 1</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">TEXT HERE</p>
+            <a href="#" class="card-link">
+              Journal Here
+            </a>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card-body">
+            <h5 class="card-title">Day 2</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">TEXT HERE</p>
+            <a href="#" class="card-link">
+              Journal Here
+            </a>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card-body">
+            <h5 class="card-title">Day 3</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">TEXT HERE</p>
+            <a href="#" class="card-link">
+              Journal Here
+            </a>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card-body">
+            <h5 class="card-title">Day 4</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">TEXT HERE</p>
+            <a href="#" class="card-link">
+              Journal Here
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-3">
+          <div class="card-body">
+            <h5 class="card-title">Day 5</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">TEXT HERE</p>
+            <a href="#" class="card-link">
+              Journal Here
+            </a>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card-body">
+            <h5 class="card-title">Day 6</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">TEXT HERE</p>
+            <a href="#" class="card-link">
+              Journal Here
+            </a>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card-body">
+            <h5 class="card-title">Day 7</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">TEXT HERE</p>
+            <a href="#" class="card-link">
+              Journal Here
+            </a>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card-body">
+            <h5 class="card-title">1-Week Reflection</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">
+              Take some time after the week to reflect on it.
+            </p>
+            <a href="#" class="card-link">
+              Journal Here
+            </a>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Week;
