9ef246979997937188ffb5e4759483006078da4d
Jason Ozulumba <cjozulumba@gmail.com>
2020-11-09 21:42:54 -0500

    Got jwt token into react via axios header

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import AuthContext from "./context/AuthContext";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { setAxiosDefaults } from "./utils/axiosDefaults";
 import axios from "axios";
-import Home from "./pages/Home"
-import Footer from "./components/Footer/Footer"
-import Header from "./components/Header/Header"
+import Home from "./pages/Home";
+import Footer from "./components/Footer/Footer";
+// import Header from "./components/Header/Header";
 
 function App() {
+  const [jwt, setJwt] = useState("");
+
   useEffect(() => {
     console.log("Make an API call");
     axios
@@ -16,11 +21,31 @@ function App() {
         console.log(err);
       });
   }, []);
+
+  useEffect(() => {
+    const localJwt = localStorage.getItem("jwt");
+    if (localJwt) {
+      setJwt(localJwt);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (jwt) {
+      setAxiosDefaults(jwt);
+      localStorage.setItem("jwt", jwt);
+    }
+  }, [jwt]);
+
   return (
-    <div className="App">
-      <Header/>
-      <Home/>
-      <Footer/>
+    <div className='App'>
+      <Router>
+        <AuthContext.Provider value={{ jwt, setJwt }}>
+          <Switch>
+            <Route exact path='/' component={Home} />
+          </Switch>
+          <Footer />
+        </AuthContext.Provider>
+      </Router>
     </div>
   );
 }
