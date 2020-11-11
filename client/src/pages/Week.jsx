d4cf0f51a88590cc53163698d10dc28399905849
Eduardo Pineda <edibozu@gmail.com>
2020-11-11 13:09:47 -0500

    linked day journal to week pages

--- client/src/pages/Week.jsx
+++ client/src/pages/Week.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./pages.css";
-import Header from "../components/Header/Header";
+import { Link } from "react-router-dom"
 
 const Week = () => {
   return (
@@ -12,79 +12,61 @@ const Week = () => {
             <div className="col-md-3">
               <div className="card-body" id="home-card-2">
                 <h5 className="card-title">Day 1</h5>
-                <p className="card-text">TEXT HERE</p>
-                <a href="" className="card-link">
-                  Open your journal...
-                </a>
+                <p className="card-text">Plan your day, and share your thougts</p>
+                <Link to="/dayjournal" className="dj-link">Open your journal...</Link>
               </div>
             </div>
             <div className="col-md-3">
               <div className="card-body" id="home-card-2">
                 <h5 className="card-title">Day 2</h5>
-                <p className="card-text">TEXT HERE</p>
-                <a href="" className="card-link">
-                  Open your journal...
-                </a>
+                <p className="card-text">Plan your day, and share your thougts</p>
+                <Link to="/dayjournal" className="dj-link">Open your journal...</Link>
               </div>
             </div>
             <div className="col-md-3">
               <div className="card-body" id="home-card-2">
                 <h5 className="card-title">Day 3</h5>
-                <p className="card-text">TEXT HERE</p>
-                <a href="" className="card-link">
-                  Open your journal...
-                </a>
+                <p className="card-text">Plan your day, and share your thougts</p>
+                <Link to="/dayjournal" className="dj-link">Open your journal...</Link>
               </div>
             </div>
             <div className="col-md-3">
               <div className="card-body" id="home-card-2">
                 <h5 className="card-title">Day 4</h5>
-                <p className="card-text">TEXT HERE</p>
-                <a href="" className="card-link">
-                  Open your journal...
-                </a>
+                <p className="card-text">Plan your day, and share your thougts</p>
+                <Link to="/dayjournal" className="dj-link">Open your journal...</Link>
               </div>
             </div>
           </div><br /><br />
           <div className="row">
-            <div className="col-md-3">
+          <div className="col-md-3">
               <div className="card-body" id="home-card-2">
                 <h5 className="card-title">Day 5</h5>
-                <p className="card-text">TEXT HERE</p>
-                <a href="" className="card-link">
-                  Open your journal...
-                </a>
+                <p className="card-text">Plan your day, and share your thougts</p>
+                <Link to="/dayjournal" className="dj-link">Open your journal...</Link>
               </div>
             </div>
             <div className="col-md-3">
               <div className="card-body" id="home-card-2">
                 <h5 className="card-title">Day 6</h5>
-                <p className="card-text">TEXT HERE</p>
-                <a href="" className="card-link">
-                  Open your journal...
-                </a>
+                <p className="card-text">Plan your day, and share your thougts</p>
+                <Link to="/dayjournal" className="dj-link">Open your journal...</Link>
               </div>
             </div>
             <div className="col-md-3">
               <div className="card-body" id="home-card-2">
                 <h5 className="card-title">Day 7</h5>
-                <p className="card-text">TEXT HERE</p>
-                <a href="" className="card-link">
-                  Open your journal...
-                </a>
+                <p className="card-text">Plan your day, and share your thougts</p>
+                <Link to="/dayjournal" className="dj-link">Open your journal...</Link>
               </div>
             </div>
             <div className="col-md-3">
               <div className="card-body" id="home-card-2">
-                <h5 className="card-title">1 Week</h5>
-                <p className="card-text">
-                  Take some time to reflect on the week.
-                </p>
-                <a href="" className="card-link">
-                  Open your journal...
-                </a>
-              </div><br /><br /><br />
-            </div>
+                <h5 className="card-title">Week</h5>
+                <p className="card-text">Take some time to reflect on your week</p>
+                <Link to="/dayjournal" className="dj-link">Open your journal...</Link>
+              </div><br/><br/>
+            </div><br/><br/>
           </div>
         </div>
       </div>
