f51d47a289fdadd7418c94143f3a5933458b4ff3
Eduardo Pineda <edibozu@gmail.com>
2020-11-13 18:37:32 -0500

    testing deployment

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -4,9 +4,9 @@ import "./Footer.css"
 const Footer = () => {
     return (
         <div>
-            <div className="footer text-center">
+            <footer className="footer text-center">
                 <p>Copyright <i className="far fa-copyright" id="cr-text"> The Borons 2020, All Rights Reserved.</i></p>
-            </div>
+            </footer>
         </div>
     );
 };
