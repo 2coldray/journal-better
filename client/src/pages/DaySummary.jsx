aa2c1c43bbea89f28534d4ecf55207b5303e2479
Jason Ozulumba <cjozulumba@gmail.com>
2020-11-15 13:00:06 -0500

    Comparison Notes model works

--- client/src/pages/DaySummary.jsx
+++ client/src/pages/DaySummary.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useContext } from "react";
-import { Card, Button, ListGroup } from "react-bootstrap";
+import { Card, ListGroup } from "react-bootstrap";
 import API from "../utils/API";
 import Header from "../components/Header/Header";
 import axios from "axios";
@@ -16,59 +16,32 @@ const DaySummary = (props) => {
     API.getTodayNotes(decoded._id).then((res) => {
       setInputList(res.data.data);
     });
-  }, [REACT_APP_SECRET, jwt]);
+    axios
+      .get(`/api/compareNotes/${decoded._id}/${props.location.Date}`)
+      .then((res) => {
+        console.log(res);
+        setInputList2(res.data.data);
+      });
+  }, [REACT_APP_SECRET, jwt, props.location.Date]);
 
-  
   const [name, setName] = useState("");
+  const [name2, setName2] = useState("");
   const [userPlans, setUserPlans] = useState("");
+  const [userPlans2, setUserPlans2] = useState("");
   const [inputList, setInputList] = useState([]);
   const [inputList2, setInputList2] = useState([]);
 
+  // const [compare, setCompare] = useState(false);
 
   const handleNameChange = (e) => {
     let { value } = e.target;
     setName(value);
   };
 
-  // const handleInputChange = (e, index = 0) => {
-  //   const { name, value } = e.target;
-  //   setTimeBlock(e.target.value);
-  // };
-
-  // const handleInputChange2 = (e, index = 0) => {
-  //   const { name, value } = e.target;
-  //   setTimeBlock2(e.target.value);
-  // };
-
-  // handle click event of the Remove button
-  const handleRemoveClick = (index) => {
-    const list = [...inputList];
-    list.splice(index, 1);
-    setInputList(list);
+  const handleNameChange2 = (e) => {
+    let { value } = e.target;
+    setName2(value);
   };
-  // const handleRemoveClick2 = (index2) => {
-  //   const list2 = [...inputList];
-  //   list2.splice(index2, 1);
-  //   setInputList2(list2);
-  // };
-  // const [savedList, setSavedList] = useState([]);
-  // handle click event of the Add button
-
-  // const handleAddClick = () => {
-  //   setInputList([...inputList, timeBlock]);
-  // };
-
-  // const handleAddClick2 = () => {
-  //   setInputList2([...inputList2, timeBlock2]);
-  // };
-  // const handleItemsSubmit = (list) => {
-  //   console.log(list);
-  //   API.postListItems(list).then((res) => {
-  //     setSavedList(res.data);
-  //     setTimeBlock("");
-  //   });
-  //   // setInputList([]);
-  // };
 
   const addNewNote = (e) => {
     e.preventDefault();
@@ -93,60 +66,55 @@ const DaySummary = (props) => {
       });
   };
 
-  const deleteNote = (id) => {
-    axios.delete(`/api/deleteNote/${id}`).then()
-  } 
+  const deleteNote = (id, index) => {
+    axios.delete(`/api/deleteNote/${id}`).then((res) => {
+      const list = [...inputList];
+      list.splice(index, 1);
+      setInputList(list);
+    });
+  };
+
+  const addCompare = (e) => {
+    e.preventDefault();
+    const decoded = jwtModule.verify(jwt, REACT_APP_SECRET);
+    console.log("I was clicked");
+    axios
+      .post(`/api/addCompare/${decoded._id}`, {
+        user_plans: userPlans2,
+        name: name2,
+        datetime: props.location.Date,
+      })
+      .then((res) => {
+        console.log(res);
+        if (res.status === 200) {
+          console.log(res.data.data);
+          setInputList2(res.data.data);
+          setName2("");
+          setUserPlans2("");
+        } else {
+          console.error();
+        }
+      });
+  };
+
+  const deleteCompare = (id, index) => {
+    axios.delete(`/api/deleteCompare/${id}`).then((res) => {
+      console.log(res);
+      const list = [...inputList2];
+      list.splice(index, 1);
+      setInputList2(list);
+    });
+  };
 
   const handleUserPlans = (e) => {
     let { value } = e.target;
     setUserPlans(value);
-    // TODO: set date time here always
   };
 
-  /* Phil's stuff he added 
-  const [item, setItem] = useState({
-    item:"",
-    title:""
-  });
-  const[philsList, setPhilsList] = useState([]);
-  const handleItemChange = (e)=>{
-    setItem(e.target.value);
-  }
-  const handleFormSubmit = (e)=>{
-    e.preventDefault();
-    API.postItem(item.item, item.title, <user_id>)
-    .then(res=> console.log(res.data));
-  }
-  */
-
-  //   {inputList.length ? inputList.map((x, i) => {
-  //     return (
-  //       <div className="box">
-  //         <input
-  //           name="Time Block"
-  //           placeholder="Enter time block notes"
-  //           value={x.timeBlock}
-  //           onChange={e => handleInputChange(e, i)}
-  //         />
-  //       <div className="btn-box">
-  //         {inputList.length !== 1 && <button
-  //           className="mr10"
-  //           onClick={() => handleRemoveClick(i)}>Remove</button>}
-  //       </div>
-  //       </div>
-  //     );
-  //   }):<div className="box">
-  //   <input
-  //     name="Time Block"
-  //     placeholder="Enter time block notes"
-  //   />
-  // <div className="btn-box">
-  //   {inputList.length !== 1 && <button
-  //     className="mr10"
-  //   onClick={() => handleRemoveClick()}>Remove</button>}
-  //   {/*inputList.length - 1 === i && <button onClick={handleAddClick}>Add</button>*/}
-  // </div>
-  // </div>}
+  const handleUserPlans2 = (e) => {
+    let { value } = e.target;
+    setUserPlans2(value);
+  };
 
   return (
     <>
@@ -169,12 +137,14 @@ const DaySummary = (props) => {
                           </div>
                           <div className='btn-box'>
                             {inputList.length !== 1 && (
-                              <button
-                                className='mr10 btn btn-danger'
-                                onClick={() => handleRemoveClick(i)}
-                              >
-                                Remove
-                              </button>
+                              <>
+                                <button
+                                  className='mr10 btn btn-danger'
+                                  onClick={() => deleteNote(x._id, i)}
+                                >
+                                  Remove
+                                </button>
+                              </>
                             )}
                           </div>
                         </Card.Text>
@@ -214,21 +184,66 @@ const DaySummary = (props) => {
               </Card.Body>
             </Card>
           </div>
+
           <div id='Compare' className='col-5 mt-4'>
             <Card className='shadow-lg'>
-              <Card.Header>How did you live today?</Card.Header>
+              <Card.Header as='h4'>How did you live today?</Card.Header>
               <Card.Body>
-                <Card.Title>Special title treatment</Card.Title>
+                <Card.Title as='h5'>Special title treatment</Card.Title>
                 <ListGroup>
-                  <Card.Text>
-                    With supporting text below as a natural lead-in to
-                    additional content.
-                  </Card.Text>
+                  {inputList2.length ? (
+                    inputList2.map((x, i) => (
+                      <ListGroup.Item key={i}>
+                        <Card.Title as='h6'>{x.name}</Card.Title>
+                        <Card.Text>
+                          <div className='box'>
+                            <p>{x.user_plans}</p>
+                          </div>
+                          <div className='btn-box'>
+                            {inputList2.length !== 1 && (
+                              <button
+                                className='mr10 btn btn-danger'
+                                onClick={() => deleteCompare(x._id, i)}
+                              >
+                                Remove
+                              </button>
+                            )}
+                          </div>
+                        </Card.Text>
+                      </ListGroup.Item>
+                    ))
+                  ) : (
+                    <ListGroup.Item>
+                      <Card.Text>How did your plans end up?</Card.Text>
+                    </ListGroup.Item>
+                  )}
+                  <form onSubmit={addCompare}>
+                    <div class='form-group mb-n4'>
+                      <label>Activity Name:</label>
+                      <input
+                        type='name'
+                        class='form-control'
+                        placeholder='Enter Activity'
+                        value={name2}
+                        onChange={handleNameChange2}
+                      />
+                    </div>
+                    <div class='form-group'>
+                      <label>Your Plans:</label>
+                      <textarea
+                        class='form-control'
+                        rows='3'
+                        value={userPlans2}
+                        onChange={handleUserPlans2}
+                      ></textarea>
+                    </div>
+                    <button type='submit' className='btn btn-primary'>
+                      {" "}
+                      Add Compare Note
+                    </button>
+                  </form>
                 </ListGroup>
               </Card.Body>
-              <Card.Footer>
-                <Button variant='primary'>Go somewhere</Button>
-              </Card.Footer>
             </Card>
           </div>
         </div>
